9184bb3c97d0857be2f1d858913c9f1f43b9a050
ManuNunez <manu.nunez.2004@gmail.com>
2024-05-04 19:48:43 -0600

    dragon solved

---
+++ dragons.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ss second
+#define ff first
+#define pb push_back
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+#define enl "\n"
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define sz(s) int(s.size())
+#define fore(i, n) for (int i = 0; i < n; ++i)
+#define forn(i, a, b) for (int i = a; i < b; ++i)
+#define ford(i, a, b) for (int i = a; i >= b; --i)
+#define arre cout << "YES" << enl;
+#define nel cout << "NO" << enl;
+#define neg cout << -1 << enl;
+#define srt(s) sort(all(s));
+#define rsrt(s) sort(rall(s));
+#define lower(vec, s) lower_bound(all(vec), s)
+#define upper(vec, s) upper_bound(all(vec), s)
+
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<char> vchar;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int MOD = 1000000007;
+const double PI = acos(-1.0);
+
+int main() {
+    fastio;
+
+    int s, n;
+    cin >> s >> n;
+
+    vector<ii> dragons(n);
+    fore(i, n) {
+        cin >> dragons[i].ff >> dragons[i].ss;
+    }
+
+    sort(all(dragons));
+
+    fore(i, n) {
+        if (s > dragons[i].ff) {
+            s += dragons[i].ss;
+        } else {
+            cout << "NO" << enl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << enl;
+
+    return 0;
+}
